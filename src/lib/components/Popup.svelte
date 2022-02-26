490c04ded1b65df27d55fc0d8324c1e6495bb74c
Kevin POIROT <kevin.poirot@epitech.eu>
2022-02-26 14:15:32 +0100

    refacto(popup): what a huge pain in the *ss

--- src/lib/components/Popup.svelte
+++ src/lib/components/Popup.svelte
@@ -4,29 +4,43 @@
 	export let close: () => void;
 </script>
 
-<div class="popup size-{size}" class:show>
+<div class="popup size-{size}" class:show class:hide={!show}>
 	<div class="popup-close" on:click={close}>❌</div>
 	<slot />
 </div>
 
 <style>
 	.popup {
-		position: absolute;
-		top: -100vh;
 		left: 0;
 		right: 0;
+		padding: 0.5rem;
+		position: absolute;
 		text-align: center;
 		font-family: 'Cascadia Code', monospace;
 		font-size: 0.8rem;
-		background: var(--theme-cardglass);
-		display: flex;
-		flex-direction: column;
 		justify-content: center;
 		justify-items: center;
-		align-items: center;
 		margin: 0 auto;
-		transition: top 0.2s ease-in;
 		z-index: 10;
+		display: flex;
+		border-radius: 25px;
+		align-items: center;
+		align-content: space-around;
+		flex-direction: column;
+		color: var(--theme-text);
+		box-shadow: var(--theme-shadow);
+		background-color: var(--theme-background);
+	}
+
+	.show {
+		top: 20vh;
+		transition: all 0.3s ease-out;
+	}
+
+	.hide {
+		top: -100vh;
+		height: 1vh;
+		transition: all 0.3s ease-in;
 	}
 
 	.popup-close {
@@ -45,11 +59,6 @@
 		background-color: var(--theme-background);
 	}
 
-	.show {
-		top: 20vh;
-		transition: top 0.2s ease-out;
-	}
-
 	.size-lg,
 	.size-md {
 		width: 50vw;
@@ -65,17 +74,22 @@
 	}
 
 	@media screen and (max-width: 768px) {
+		.show {
+			top: 10vh;
+			transition: all 0.3s ease-out;
+		}
+
 		.size-lg,
 		.size-md {
 			width: 90vw;
 		}
 
 		.size-lg {
-			height: 60vh;
+			height: 80vh;
 		}
 
 		.size-md {
-			height: 25vh;
+			height: 45vh;
 		}
 	}
 </style>
