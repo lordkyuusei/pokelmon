cc20f69330e8fb65dcb0be9fe3d3e073df62e66d
Kyuu <me@lkyuusei.fr>
2023-11-30 21:46:22 +0100

    chore(i18n): remove aill "daily" occurences

--- src/lib/store/i18n.ts
+++ src/lib/store/i18n.ts
@@ -2,8 +2,8 @@ import { writable, derived } from "svelte/store";
 
 const translations = {
     english: {
-        "game-goal": "Guess the team of six pokémon of the day in less than {{ MAX_TRIALS }} trials",
-        "game-dupe": "You found a duplicate pokémon! Another one is hiding in today's challenge.",
+        "game-goal": "Guess the team of six pokémon in less than {{ MAX_TRIALS }} trials",
+        "game-dupe": "You found a duplicate pokémon! Another one is hiding in the challenge.",
         "game-won": "You won!",
         "game-lost": "You lost...",
         "game-copy": "Copy result",
@@ -21,21 +21,21 @@ const translations = {
         "settings-accessibility-on": "On",
         "settings-accessibility-off": "Off",
         "settings-close": "Close",
-        "settings-rules": "Rules",
-        "settings-rules-1": "You know how it works around here :). Take a look at the color scheme:",
-        "settings-rules-correct": "Green background: present & right position.",
-        "settings-rules-misplaced": "Orange background: present but wrong position.",
-        "settings-rules-wrong": "Red background: not present in the team.",
-        "settings-rules-clue1": "Clue 1: There will never be Pokemon duplicates side-by-side.",
+        "settings-rules": "Game's rules",
+        "settings-rules-1": "Take a look at the color scheme:",
+        "settings-rules-correct": "Green background: in the team & right position.",
+        "settings-rules-misplaced": "Orange background: in the team but wrong position.",
+        "settings-rules-wrong": "Red background: not in the team.",
+        "settings-rules-clue1": "Rule 1: There will never be Pokemon duplicates side-by-side.",
         "settings-rules-clue2": "Clue 2: There will never be Pokemon surrounded by super-effective typing against it.",
         "settings-items": "Items",
         "settings-items-desc": "With each guess, you can sometimes get an item. There are currently two items:",
-        "settings-items-1": "💊 Potion - Removes your last guess from the board, granting you a new try.",
+        "settings-items-1": "💊 Potion - Removes your first guess from the board, granting you a new try.",
         "settings-items-2": "🔴 Pokeball - Reveals the Pokemon from the solution at your current guess cell.",
     },
     french: {
-        "game-goal": "Devinez l'équipe de six pokémon du jour en moins de {{ MAX_TRIALS }} essais",
-        "game-dupe": "Vous avez trouvé un doublon ! Un autre est caché dans le challenge du jour.",
+        "game-goal": "Devinez l'équipe de six pokémon en moins de {{ MAX_TRIALS }} essais",
+        "game-dupe": "Vous avez trouvé un doublon ! Un autre est caché dans le challenge.",
         "game-won": "Victoire !",
         "game-lost": "Défaite...",
         "game-copy": "Copier le résultat",
@@ -49,24 +49,25 @@ const translations = {
         "settings-lang": "Langue",
         "settings-lang-en": "Anglais",
         "settings-lang-fr": "Français",
-        "settings-accessibility": "Mode Accessibilité",
+        "settings-accessibility": "Accessibilité",
         "settings-accessibility-on": "Activé",
         "settings-accessibility-off": "Désactivé",
-        "settings-close": "Close",
-        "settings-rules": "Règles",
-        "settings-rules-1": "Vous savez comment ça marche par ici :). Voici le code couleur utilisé:",
+        "settings-close": "Fermer",
+        "settings-rules": "Règles du jeu",
+        "settings-rules-1": "Voici le code couleur utilisé:",
         "settings-rules-correct": "Fond vert: le Pokémon est présent et positionné correctement.",
         "settings-rules-misplaced": "Fond orange: le Pokémon est présent mais mal positionné.",
         "settings-rules-wrong": "Fond rouge: le Pokémon n'est pas présent dans l'équipe.",
-        "settings-rules-clue1": "Indice 1 : Il n'y aura jamais deux Pokémon identiques côte à côte.",
-        "settings-rules-clue2": "Indice 2 : Il n'y aura jamais de Pokémon entouré par des congénères avec un type avantageux contre lui.",
+        "settings-rules-clue1": "Règle 1: Il n'y aura jamais deux Pokémon identiques côte à côte.",
+        "settings-rules-clue2": "Règle 2: Il n'y aura jamais de Pokémon entouré par des congénères avec un type super efficace contre lui.",
         "settings-items": "Objets",
         "settings-items-desc": "Avec chaque essai, vous pouvez parfois obtenir un objet. Il y a actuellement deux objets:",
         "settings-items-1": "💊 Potion - Supprime votre dernier essai du plateau.",
         "settings-items-2": "🔴 Pokeball - Révèle le Pokémon à l'emplacement du curseur.",
     }
 }
-export const locale = writable("english");
+
+export const locale = writable("french");
 export const locales = Object.keys(translations);
 
 const translate = (locale: string, key: string, vars: { [x: string]: any; }): string => {
